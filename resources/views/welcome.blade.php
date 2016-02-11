e7556e3640d2ad4d234c02f617991de3100a8429
Bruno Nogueira Martins <bn.martins02@gmail.com>
2016-02-11 16:56:52 -0200

    -
    -

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -5,10 +5,10 @@
     <div class="row">
         <div class="col-md-10 col-md-offset-1">
             <div class="panel panel-default">
-                <div class="panel-heading">Welcome</div>
+                <div class="panel-heading">Seja Bem Vindo!</div>
 
                 <div class="panel-body">
-                    Your Application's Landing Page.
+                    Faça o login para ter acesso ao painel administrativo!
                 </div>
             </div>
         </div>
